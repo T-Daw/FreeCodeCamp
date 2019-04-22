aee25b20138a78d22b4ab28e5ec6cca7b4782ccb
Tasneem Dawoodjee <tdawoodjee@gmail.com>
2019-04-22 10:14:22 -0500

    adding reverse a string function

---
+++ reverse_a_string.js
@@ -0,0 +1,20 @@
+function reverseString(str) {
+
+
+    str_len = str.length ;
+    str_arr = [];
+    rev_str = '';
+
+  
+    for(i = str_len ; i > -1 ; i = i-1 ) {
+      str_arr.push(str[i])
+    }
+    str_arr.shift();
+
+    for (i = 0 ; i < str_len ; i ++ ) {
+      rev_str = rev_str + str_arr[i];
+    }
+    return rev_str;
+
+  }
+console.log(reverseString("hello"));
